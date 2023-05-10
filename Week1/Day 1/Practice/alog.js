652436f704fc7ceb194dadb77a0c48dfdfc01909
Aymenbenmoussa59 <aymenbenmoussa59@gmail.com>
2023-05-10 11:39:24 +0100

    flexiblecolumns

---
+++ Week1/Day 1/Practice/alog.js
@@ -0,0 +1,10 @@
+var arr=["a", "b", "c", "d", "e"];
+var x=["e", "d", "c", "b", "a"];
+while(x=arr){
+    arr[0]=x[0]    
+    arr[1]=x[1]
+    arr[3]=x[3]
+    arr[4]=x[4]
+    console.log
+
+}
---
+++ Week2/Day 1/Practice/fizzbuzz.js
@@ -0,0 +1,21 @@
+
+for (let i=0;i<100;i++){
+    if(i%3==0){
+        console.log ("fizz");
+    }
+    if(i%5==0){
+        console.log ("buzz");
+
+    }
+    if(i%5==0&i%3==0){
+        console.log("fizzbuzz")
+    }
+
+
+
+
+
+}
+
+
+
